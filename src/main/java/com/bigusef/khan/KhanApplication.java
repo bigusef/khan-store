4a93d3282dcbd070542f2ec7d4067b9ba36cebfb
Mahmud Youssef <mahmud.youssef@icloud.com>
2023-07-09 19:44:28 +0300

    initialize project data

---
+++ src/main/java/com/bigusef/khan/KhanApplication.java
@@ -0,0 +1,13 @@
+package com.bigusef.khan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bigusef/khan/KhanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigusef.khan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
